41ce1a558c9971f4d2796129c172890b1fecbc05
[M210®] <m210-2007@mail.ru>
2021-11-15 13:59:11 +0300

    New branch

--- src/ru/m210projects/Main.java
+++ src/ru/m210projects/Main.java
@@ -3,6 +3,6 @@ package ru.m210projects;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World!!! in develop branch");
     }
 }
